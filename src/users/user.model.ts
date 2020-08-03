38e90ccffdabe3eb6003f823e9e8ceff4cbc9eef
cdimitroulas <c.dimitroulas@gmail.com>
2020-08-03 18:30:19 +0200

    Remove commented out code which is not needed

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -152,37 +152,6 @@ export const createUser = (input: CreateUserPayload) => (deps: CreateUserDeps): 
   )
 }
 
-// UserSchema.methods.toAuthJSON = function(){
-//   return {
-//     username: this.username,
-//     email: this.email,
-//     token: this.generateJWT(),
-//     bio: this.bio,
-//     image: this.image
-//   };
-// };
-
-// var UserSchema = new mongoose.Schema({
-//   username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-//   email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-//   bio: String,
-//   image: String,
-//   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
-//   following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-//   hash: String,
-//   salt: String
-// }, {timestamps: true});
-
-// UserSchema.methods.toAuthJSON = function(){
-//   return {
-//     username: this.username,
-//     email: this.email,
-//     token: this.generateJWT(),
-//     bio: this.bio,
-//     image: this.image
-//   };
-// };
-
 // UserSchema.methods.toProfileJSONFor = function(user){
 //   return {
 //     username: this.username,
